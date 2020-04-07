b7fca3104cd8739b800934fa972445a659e6d935
forlouman <1061781184@qq.com>
2020-04-07 22:37:24 +0800

    啦啦啦

--- Base/BaseControllers/JMBaseViewController.h
+++ Base/BaseControllers/JMBaseViewController.h
@@ -10,11 +10,11 @@
 #import "JMRequestBaseViewController.h"
 
 
-@interface JMBaseViewController : JMRequestBaseViewController
--(instancetype)initWithStoryboard;
-//仅用于子类initWithStoryboard方法中，不可外用
-- (instancetype)initWithStoryboardName:(NSString *)storyboardName;
-//子类重写用于处理控件初始化属性
+//@interface JMBaseViewController : JMRequestBaseViewController
+//-(instancetype)initWithStoryboard;
+////仅用于子类initWithStoryboard方法中，不可外用
+//- (instancetype)initWithStoryboardName:(NSString *)storyboardName;
+////子类重写用于处理控件初始化属性
 -(void)initControl;
 //子类重写用于处理初始化数据
 -(void)initData;
